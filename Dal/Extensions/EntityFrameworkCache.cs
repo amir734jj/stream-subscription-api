2fd25a736d73e43b4019154b513873190cbca8e4
Amir <seyedhesamian@northwesternmutual.com>
2018-08-06 15:27:23 -0500

    Use cache for EF

---
+++ Dal/Extensions/EntityFrameworkCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Extensions
+{
+    public static class EntityFrameworkCache
+    {
+        public static TEntity FirstOrDefaultCache<TEntity>(this DbSet<TEntity> queryable, Expression<Func<TEntity, bool>> condition) 
+            where TEntity : class
+        {
+            return queryable.Local.FirstOrDefault(condition.Compile()) // Find in local cache
+                   ?? queryable.FirstOrDefault(condition);             // If local cache returns null check the db
+        }
+    }
+}
