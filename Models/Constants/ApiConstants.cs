4da99a60c97f3393e872163183e1249ec6ed63ff
Amir <hesamian@uwm.edu>
2020-01-06 23:25:03 -0600

    More progress

--- Models/Constants/ApiConstants.cs
+++ Models/Constants/ApiConstants.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace Models.Constants
 {
     public static class ApiConstants
     {
         public const string AuthenticationSessionCookieName = "streaming-subscription";
+
+        public static readonly string[] AdminEmail = {  "amirhesamyan@gmail.com"};
     }
 }
