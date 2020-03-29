a11f91413a7390d9703754ca46c639a297e4eb51
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-03-29 12:00:52 -0500

    Better state

---
+++ Logic/Logic/FilterSongLogic.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Logic.Interfaces;
+
+namespace Logic.Logic
+{
+    public class FilterSongLogic : IFilterSongLogic
+    {
+        public bool ShouldInclude(string filename, string filter)
+        {
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                return true;
+            }
+
+            var flag = filter.Split(Environment.NewLine)
+                .Where(pattern => !string.IsNullOrWhiteSpace(pattern))
+                .Any(pattern => !Regex.Matches(filename, pattern, RegexOptions.IgnoreCase).Any());
+
+            return flag;
+        }
+    }
+}
