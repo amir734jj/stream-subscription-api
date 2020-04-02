84e6213528b5a968e5ae90dac2ee049654dfaedf
Amir <hesamian@uwm.edu>
2020-04-02 01:51:14 -0500

    Revert "Updated stream version"
    This reverts commit acccf3188053d2f040ae9aa0241203782f4bcdd2.

--- Logic/Services/MessageHub.cs
+++ Logic/Services/MessageHub.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Logic.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
@@ -18,25 +17,17 @@ namespace Logic.Services
         // Connected IDs
         private static readonly IDictionary<string, User> Users = new ConcurrentDictionary<string, User>();
 
-        private readonly Lazy<IStreamRipperManager> _streamRipperManager;
-
-        public MessageHub(UserManager<User> userManager, Lazy<IStreamRipperManager> streamRipperManager)
+        public MessageHub(UserManager<User> userManager)
         {
             _userManager = userManager;
-            _streamRipperManager = streamRipperManager;
         }
             
         public override async Task OnConnectedAsync()
         {
-            var user =  await _userManager.FindByNameAsync(Context.User.Identity.Name);
-
-            Users.Add(Context.ConnectionId, user);
+            Users.Add(Context.ConnectionId, await _userManager.FindByNameAsync(Context.User.Identity.Name));
 
             await Clients.All.SendAsync("log", "joined", Context.ConnectionId);
             await Clients.All.SendAsync("count", Users.Count);
-
-            await Clients.Client(Context.ConnectionId)
-                .SendAsync("streams", await _streamRipperManager.Value.For(user).Status());
         }
 
         public override async Task OnDisconnectedAsync(Exception ex)
