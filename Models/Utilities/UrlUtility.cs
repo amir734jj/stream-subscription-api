78752bafe847a7702f81a3ab18cf87812cbc6739
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-04-04 02:03:25 -0500

    Minor

--- Models/Utilities/UrlUtility.cs
+++ Models/Utilities/UrlUtility.cs
@@ -23,8 +23,8 @@ namespace Models.Utilities
             var connectionStringBuilder = new Dictionary<string, string>
             {
                 ["Host"] = url.Host,
-                ["Username"] = url.UserInfo.Split(':')[0],
-                ["Password"] = url.UserInfo.Split(':')[1],
+                ["Username"] = url.UserInfo.Split(':').GetValue(0)?.ToString(),
+                ["Password"] = url.UserInfo.Split(':').GetValue(1)?.ToString(),
                 ["Database"] = url.LocalPath.Substring(1),
                 ["ApplicationName"] = "stream-subscription-api"
             };
