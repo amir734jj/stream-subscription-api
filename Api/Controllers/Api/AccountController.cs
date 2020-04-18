e6d4927cced34adaae3b18137708bec018039c63
Amir <hesamian@uwm.edu>
2020-04-18 03:20:06 -0500

    Maybe fixed the setup

--- Api/Controllers/Api/AccountController.cs
+++ Api/Controllers/Api/AccountController.cs
@@ -72,7 +72,7 @@ namespace Api.Controllers.Api
 
             if (identityResults.All(x => x.Succeeded))
             {
-                await _userSetup.Setup(user.UserName);
+                await Task.Delay(TimeSpan.FromSeconds(5)).ContinueWith(t => _userSetup.Setup(user.UserName));
                 
                 return Ok("Successfully registered!");
             }
