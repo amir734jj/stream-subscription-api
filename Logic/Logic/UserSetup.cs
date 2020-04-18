aea297c9b145729d92c180db4f0b1dad4e7ca56a
Amir <hesamian@uwm.edu>
2020-04-18 02:57:02 -0500

    Maybe fixed the setup

--- Logic/Logic/UserSetup.cs
+++ Logic/Logic/UserSetup.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Logic.Interfaces;
-using Models.Models;
 using Newtonsoft.Json;
+using StructureMap;
 using Stream = Models.Models.Stream;
 using static Models.Constants.ApplicationConstants;
 
@@ -12,30 +12,31 @@ namespace Logic.Logic
 {
     public class UserSetup : IUserSetup
     {
-        private readonly IStreamLogic _streamLogic;
-        
-        private readonly IStreamRipperManager _streamRipperManager;
+        private readonly Container _container;
 
-        private readonly IUserLogic _userLogic;
-
-        public UserSetup(IStreamLogic streamLogic, IStreamRipperManager streamRipperManager, IUserLogic userLogic)
+        public UserSetup(Container container)
         {
-            _streamLogic = streamLogic;
-            _streamRipperManager = streamRipperManager;
-            _userLogic = userLogic;
+            _container = container;
         }
-        
+
         public async Task Setup(string username)
         {
-            var user = (await _userLogic.GetAll()).First(x => x.UserName == username);
-            
+            using var nestedContainer = _container.GetNestedContainer();
+
+            var streamRipperManager = nestedContainer.GetInstance<IStreamRipperManager>();
+
+            var userLogic = nestedContainer.GetInstance<IUserLogic>();
+            var streamLogic = nestedContainer.GetInstance<IStreamLogic>();
+
+            var user = (await userLogic.GetAll()).First(x => x.UserName == username);
+
             var fileString = await File.ReadAllTextAsync(SetupUserRecipe);
 
             await Task.WhenAll(JsonConvert.DeserializeAnonymousType(fileString, new {Streams = new List<Stream>()})
                 .Streams
-                .Select(x => _streamLogic.For(user).Save(x)));
+                .Select(x => streamLogic.For(user).Save(x)));
 
-            await _streamRipperManager.StartMany(await _streamLogic.For(user).GetAll());
+            await streamRipperManager.StartMany(await streamLogic.For(user).GetAll());
         }
     }
 }
