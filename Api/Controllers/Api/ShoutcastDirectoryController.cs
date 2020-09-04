86b0bf4d8a999d441179767d357d3700b06fd709
Amir <hesamian@uwm.edu>
2020-09-04 12:17:31 -0500

    Added route to return genres

--- Api/Controllers/Api/ShoutcastDirectoryController.cs
+++ Api/Controllers/Api/ShoutcastDirectoryController.cs
@@ -26,12 +26,33 @@ namespace Api.Controllers.Api
 
             return Ok(url);
         }
+
+        [HttpGet]
+        [Route("genres")]
+        public IActionResult Genres()
+        {
+            var genres = _shoutcastDirectoryApi.Result.Select(x => x.Genre).Distinct().ToList();
+
+            return Ok(genres);
+        }
         
         [HttpGet]
         [Route("")]
-        public IActionResult Collect()
+        public IActionResult Collect([FromQuery] string name = "", [FromQuery] string genre = "")
         {
             var result = _shoutcastDirectoryApi.Result;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                result = result.Where(x => x.Name.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+            
+            if (!string.IsNullOrEmpty(genre))
+            {
+                result = result.Where(x => x.Genre.Contains(genre, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            result = result.ToList();
 
             return Ok(result);
         }
