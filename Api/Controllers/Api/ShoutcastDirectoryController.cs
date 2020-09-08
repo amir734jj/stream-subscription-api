9fb1fb6a1def0f48a640c7c6e673706733431d2a
Amir <hesamian@uwm.edu>
2020-09-08 15:31:08 -0500

    Max possible options is 50

--- Api/Controllers/Api/ShoutcastDirectoryController.cs
+++ Api/Controllers/Api/ShoutcastDirectoryController.cs
@@ -54,7 +54,7 @@ namespace Api.Controllers.Api
                 result = result.Where(x => x.Genre == genre).ToList();
             }
 
-            result = result.ToList();
+            result = result.Take(50).ToList();
 
             return Ok(result);
         }
