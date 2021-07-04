005fb308561ce7e7d37cd9698c371861c435ea2f
Amir <hesamian@uwm.edu>
2021-07-04 14:09:43 -0500

    use $port

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,12 +10,15 @@ namespace Api
     {
         public static void Main(string[] args)
         {
+            if (!int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var port)) port = 5000;
+
             var host = WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Error))
                 .UseStartup<Startup>()
+                .UseUrls("http://*:" + port)
                 .ConfigureLogging((hostingContext, logging) =>
                 {
                     logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
